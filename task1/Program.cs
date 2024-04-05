9fbacdb56c5f35bcbf299174d4ba27ebd50909f1
Danylo Musiiaka <dollarone98@gmail.com>
2024-04-05 14:52:37 +0300

    added task1 & task2

---
+++ task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class MyAccessModifiers
+{
+    private int birthYear;
+    public string personalInfo; 
+    protected internal string IdNumber;
+    public static byte averageMiddleAge = 50;
+    internal string Name;
+    internal string NickName { get; set; }
+
+    public MyAccessModifiers(int birthYear, string idNumber, string personalInfo)
+    {
+        this.birthYear = birthYear;
+        this.IdNumber = idNumber;
+        this.personalInfo = personalInfo;
+        this.Name = ""; 
+        this.NickName = ""; 
+    }
+
+    public int Age
+    {
+        get { return DateTime.Now.Year - birthYear; }
+    }
+
+    protected internal bool HasLivedHalfOfLife()
+    {
+        return Age >= MyAccessModifiers.averageMiddleAge / 2;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        MyAccessModifiers other = (MyAccessModifiers)obj;
+        return this.Name == other.Name && this.Age == other.Age && this.personalInfo == other.personalInfo;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Age, personalInfo);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyAccessModifiers person = new MyAccessModifiers(1990, "ID123456", "John Doe");
+
+        person.Name = "John";
+
+        Console.WriteLine($"Name: {person.Name}");
+        Console.WriteLine($"Age: {person.Age}");
+
+        bool hasLivedHalf = person.HasLivedHalfOfLife();
+        Console.WriteLine($"Has lived half of life: {hasLivedHalf}");
+
+        Console.WriteLine($"Personal Info: {person.personalInfo}"); 
+        Console.WriteLine($"ID Number: {person.IdNumber}");
+
+        MyAccessModifiers person2 = new MyAccessModifiers(1990, "ID123456", "John Doe");
+        bool areEqual = person == person2;
+        Console.WriteLine($"Are the two persons equal: {areEqual}");
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public class Employee
+{
+    internal string name;
+    private DateTime hiringDate;
+
+    public Employee(string name, DateTime hiringDate)
+    {
+        this.name = name;
+        this.hiringDate = hiringDate;
+    }
+
+    public int Experience()
+    {
+        return (DateTime.Now - hiringDate).Days / 365;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"{name} has {Experience()} years of experience");
+    }
+}
+
+public class Developer : Employee
+{
+    private string programmingLanguage;
+
+    public Developer(string name, DateTime hiringDate, string programmingLanguage) : base(name, hiringDate)
+    {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public new void ShowInfo()
+    {
+        base.ShowInfo();
+        Console.WriteLine($"{name} is {programmingLanguage} programmer");
+    }
+}
+
+public class Tester : Employee
+{
+    private bool isAutomation;
+
+    public Tester(string name, DateTime hiringDate, bool isAutomation) : base(name, hiringDate)
+    {
+        this.isAutomation = isAutomation;
+    }
+
+    public new void ShowInfo()
+    {
+        base.ShowInfo();
+        if (isAutomation)
+        {
+            Console.WriteLine($"{name} is automated tester");
+        }
+        else
+        {
+            Console.WriteLine($"{name} is manual tester");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Employee emp = new Employee("Danya", new DateTime(2010, 1, 1));
+        emp.ShowInfo();
+
+        Developer dev = new Developer("Yura", new DateTime(2015, 5, 10), "C#");
+        dev.ShowInfo();
+
+        Tester tester1 = new Tester("Roma", new DateTime(2013, 8, 20), true);
+        tester1.ShowInfo();
+
+        Tester tester2 = new Tester("Solya", new DateTime(2014, 3, 15), false);
+        tester2.ShowInfo();
+        
+    }
+}
