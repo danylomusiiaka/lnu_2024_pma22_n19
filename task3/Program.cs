aabceac9d0261abee4087d8f9ac286f5cd193956
Danylo Musiiaka <dollarone98@gmail.com>
2024-04-05 15:14:33 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿public delegate double CalcDelegate(double num1, double num2, char operation);
+
+public class CalcProgram
+{
+    public static double Calc(double num1, double num2, char operation)
+    {
+        switch (operation)
+        {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 != 0)
+                    return num1 / num2;
+                else
+                    return 0; 
+            default:
+                throw new ArgumentException("Invalid operation");
+        }
+    }
+
+    public CalcDelegate funcCalc = new CalcDelegate(Calc);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalcProgram calculator = new CalcProgram();
+        
+        Console.WriteLine(calculator.funcCalc(5, 3, '+')); 
+        Console.WriteLine(calculator.funcCalc(5, 3, '-')); 
+        Console.WriteLine(calculator.funcCalc(5, 3, '*')); 
+        Console.WriteLine(calculator.funcCalc(5, 0, '/')); 
+        Console.WriteLine(calculator.funcCalc(5, 0, '/')); 
+    }
+}
